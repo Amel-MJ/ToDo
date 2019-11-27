19a7c1f75954438138fab8e856c7d73a70d42927
Amel Mejri <web.amel.m@gmail.com>
2019-11-27 10:29:08 +0100

    ToDo

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+function ajout() {
+    let x = document.querySelector(".add").value ;
+    document.getElementById("demo").innerHTML = x;
+    console.log(document.querySelector(".add").value)
+}
+
+
+
+
