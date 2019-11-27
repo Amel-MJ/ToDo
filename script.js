f0496734adb46ba9e7d92a2577a288efbee06f51
Amel Mejri <web.amel.m@gmail.com>
2019-11-27 12:40:26 +0100

    ToDo

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 
+   
+    
+    document.getElementById('btnsub').addEventListener('click', function(e){
+        let x = document.querySelector(".add").value;
+        let li =`<ul><li><button type="button">Complete</button></li>
+        <li><button type="button" id="item" >Delete</button></li>
+        <li>${x}</li><ul>`;
+        demoList.insertAdjacentHTML("afterend", li);
+        document.querySelector(".add").value = "";
+    })
+    
 
-function ajout() {
-    let x = document.querySelector(".add").value ;
-    document.getElementById("demo").innerHTML = x;
-    console.log(document.querySelector(".add").value)
-}
-
-
+     
 
 
