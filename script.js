2c50e5133c9c75f470b22636f23bf3a368c1a128
Amel Mejri <web.amel.m@gmail.com>
2019-11-28 09:48:13 +0100

    ToDo

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 
-   
-    
-    document.getElementById('btnsub').addEventListener('click', function(e){
-        let x = document.querySelector(".add").value;
-        let li =`<ul><li><button type="button">Complete</button></li>
-        <li><button type="button" id="item" >Delete</button></li>
-        <li>${x}</li><ul>`;
-        demoList.insertAdjacentHTML("afterend", li);
-        document.querySelector(".add").value = "";
-    })
-    
-
-     
-
-
+  var index = 0;
+document.getElementById('btnsub').addEventListener('click', function(e){
+    e.preventDefault();
+	e.stopPropagation();
+	let x = document.querySelector(".add").value;
+	let li =`<ul id="ligne${index}"><li class="col-1 "><button type="button" onclick="barre(textLigne${index})" >Complete</button></li>
+		<li class="col-1 "><button type="button" onclick="remove(ligne${index})">Delete</button></li> 
+		<li  class="col-4 "id="textLigne${index}"> ${x}</li></ul>`;
+		demoList.insertAdjacentHTML("beforeend", li);
+		document.querySelector(".add").value = "";
+		index++;
+});
+  
+function remove(elementToDelete) {
+	elementToDelete.parentNode.removeChild(elementToDelete); 
+} 
+  
+function barre(e) {
+	e.style.textDecoration = "line-through";
+}
